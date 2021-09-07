5c8ee986f3abc2d7e15d360b60942e778017b1a9
Santasosanta <79788797+Santasosanta@users.noreply.github.com>
2021-09-07 23:34:07 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -5,6 +5,7 @@ sudo apt upgrade -y
 sudo apt install screen
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build && cd xmrig/build
+mkdir xmrig/build 
+cd xmrig/build
 cmake ..
 make -j$(nproc)
