a1393ffe008848a1064bacc5c3de0c4da03ca1d4
Santasosanta <79788797+Santasosanta@users.noreply.github.com>
2021-09-07 23:45:39 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,7 +1,5 @@
-# !/bin/bash
-
-sudo apt update
-sudo apt upgrade -y
+#!/bin/bash
+echo "Hello miner"
 sudo apt install screen
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
