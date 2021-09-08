601650945e531bc734d8f239ff3765f44c386959
Santasosanta <79788797+Santasosanta@users.noreply.github.com>
2021-09-08 16:43:32 +0300

    Add files via upload

---
+++ miner.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+echo"Miner setup"
+echo"***************************************************************************"
+sudo apt install screen
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
